08396cd4f25cf5989d9976dc9984f772e19fbe6d
Omar Adrian Salinas Villanueva <31166514+omarsalinas16@users.noreply.github.com>
2018-04-07 17:58:48 -0600

    No courses messages on the dashboard.

--- src/views/app/dashboard/DashboardAchievements.vue
+++ src/views/app/dashboard/DashboardAchievements.vue
@@ -1,10 +1,18 @@
 <template>
-	<h2>Logros</h2>	
+	<div class="achievements">
+		<h2>Logros</h2>
+		<p v-show="achievements.length <= 0" class="text_color--medium">No has completado ningun logro aún, sigue intentandolo!</p>
+	</div>
 </template>
 
 <script>
 export default {
 	name: 'DashboardAchievements',
+	data() {
+		return {
+			achievements: []
+		}
+	},
 }
 </script>
 
